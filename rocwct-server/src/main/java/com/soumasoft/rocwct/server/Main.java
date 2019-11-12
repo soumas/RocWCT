db177d767f09f20d2d3d425c5956b5f9d62500a7
soumas <office@soumasoft.com>
2019-11-12 22:54:43 +0100

    Remove Log4j-Annotation from Main class to avoid initialization in install-mode

--- rocwct-server/src/main/java/com/soumasoft/rocwct/server/Main.java
+++ rocwct-server/src/main/java/com/soumasoft/rocwct/server/Main.java
@@ -1,14 +1,14 @@
 package com.soumasoft.rocwct.server;
 
+import org.apache.logging.log4j.LogManager;
+
 import com.soumasoft.rocwct.server.config.ConsoleParam;
 import com.soumasoft.rocwct.server.config.Properties;
 import com.soumasoft.rocwct.server.console.ConsolePrinter;
 import com.soumasoft.rocwct.server.util.InstallHelper;
 import com.soumasoft.rocwct.server.util.ShutdownHelper;
 
-import lombok.extern.log4j.Log4j2;
-
-@Log4j2
+//don't use Log4j in Main --> log4j2.xml may not exist in install mode
 public class Main {
 
 	public static void main(String[] args) throws Exception {
@@ -22,13 +22,13 @@ public class Main {
 		
 		// initialize properties
 		if (!Properties.init()) {
-			log.fatal("ERROR on loading settings - not able to start RocWCT server. Please check previouse messages for further details.");
+			LogManager.getLogger().fatal("ERROR on loading settings - not able to start RocWCT server. Please check previouse messages for further details.");
 			return;
 		}	
 		
 		// shutdown mode
 		if (ConsoleParam.getBoolean(ConsoleParam.SHUTDOWN)) {
-			log.info("SHUTDOWN! New RocWCT instance started for sending shutdown command to the WebSocket!");
+			LogManager.getLogger().info("SHUTDOWN! New RocWCT instance started for sending shutdown command to the WebSocket!");
 			ShutdownHelper.shutdownOtherInstance();
 			return;
 		}
